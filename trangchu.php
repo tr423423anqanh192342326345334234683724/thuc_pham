a4e35f8839f3f43d2f6451fd89df51ff02179a9d
tr423423anqanh192342326345334234683724 <tqayoutobe@gmail.com>
2024-10-18 16:16:52 +0700

    Update trangchu.php

--- trangchu.php
+++ trangchu.php
@@ -4,21 +4,73 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Thực Phẩm Trức Năng Nhóm 8</title>
     <style>
-        
+        body {
+            background-image: url('img/bg.jpg');
+            background-size: cover;
+            background-position: center;
+        }
     </style>
 </head>
 <body>
     <form action="" method="post">
-        <div>
-            <p>Thuốc luôn có, Đặt là giao đặt ngay</p>
-            <input type="text" name="search" placeholder="Nhập tên thuốc">
-            <button type="submit">Tìm kiếm</button>
-      
-        </div>
+        <p>Thuốc luôn có, Đặt là giao</p>
+        <input type="text" name="search" placeholder="Nhập tên thuốc" required>
+        <button type="submit">Tìm kiếm</button>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+    
+</form>
+<label for="image">Chọn hình ảnh:</label>
+    <input type="file" name="image" id="image" required>
+    <button type="submit">Tải lên</button>
     </form>
+    
     <?php
-    ?>
+$severname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "thuc_pham_chuc_nang";
+
+$conn = mysqli_connect($severname, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection Lỗi: " . mysqli_connect_error());
+}
+
+if (isset($_POST['search'])) {
+    $search = $_POST['search'];
+    $sql = "SELECT * FROM mat_hang WHERE ten_mat_hang LIKE '%$search%'";
+    $result = mysqli_query($conn, $sql);
+
+    // Kiểm tra lỗi truy vấn
+    if (!$result) {
+        die("Lỗi truy vấn: " . mysqli_error($conn));
+    }
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+           echo $row['loai_mat_hang'] . "<br> " . $row['ten_mat_hang'] ."<br>". $row['cong_dung_mat_hang'] ."<br>". $row['gia_mat_hang'] ."<br>". $row['hinh_anh'] ."<br>";
+        }
+    } else {
+        echo "Không tìm thấy sản phẩm nào.";
+    }
+}
+mysqli_close($conn);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $image = $_FILES['image']['tmp_name'];
+    $imageData = addslashes(file_get_contents($image));
+
+    $sql = "INSERT INTO images (image_data) VALUES ('$imageData')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Hình ảnh đã được tải lên thành công!";
+    } else {
+        echo "Lỗi: " . mysqli_error($conn);
+    }
+}
+
+mysqli_close($conn);
+?>
+?>
+
 </body>
 </html>
